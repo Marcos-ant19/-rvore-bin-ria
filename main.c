ab00597f7155aa20d04f3d7e6df662e79fb2e48e
MARCOS ANTONIO DO NASCIMENTO <Marcosantoniodonascimento609@gmail.com>
2026-06-04 10:08:13 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,18 +1,6 @@
 #include <stdio.h>
 #include "arvore.h"
 
-/*1- Uma árvore binária é uma estrutura hierarquica formada por nós, ela é criada a partir.
-da raiz, cada nó pode haver apenas 2 filhos
-um a direita e um outro a a esquerda, a árvore e umas dos estruturas de dados mas
-eficientes na hora da busca de algum dado.*/
-
-/*2- A árvore binária é uma esturtura hierárquica um exemplo seria sistema de arquivo de pasta do pc aonde eles sõa dividido de forma 
-hierárquica, um segundo exemplo seria um sistema de gereciamneto de chaves de campeonatos esportivos utilizam estruturas de árvore
-binária para organizar as partidas eliminatórias famoso "mata-mata", a raiz é o grande campeão
-ó nó raiz se divide em dois filhos que no caso são os times que jogaram a a final, cada um desses nós se divide em mais dois e assim por diante até chegar
-a primeira fase.*/
-
-
 int main(){
 
     Arvore* a = criar_arvore();
